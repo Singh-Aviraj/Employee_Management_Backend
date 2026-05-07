86ab9fe69eaa4a44df698143d61fdaf79f529c03
Aviraj Singh <148427283+Singh-Aviraj@users.noreply.github.com>
2026-05-07 17:46:50 +0530

    Update Repository.java

--- src/main/java/com/project/EmployeeManagement/Repository.java
+++ src/main/java/com/project/EmployeeManagement/Repository.java
@@ -8,3 +8,14 @@ public interface Repository extends JpaRepository<Entities, Long> {
     List<Entities> findByNameContainingIgnoreCase(String name);
     List<Entities> findById(Integer id);
 }
+package com.project.EmployeeManagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface Repository extends JpaRepository<Entities, Long> {
+    List<Entities> findByNameContainingIgnoreCase(String name);
+    List<Entities> findById(Integer id);
+}
+
